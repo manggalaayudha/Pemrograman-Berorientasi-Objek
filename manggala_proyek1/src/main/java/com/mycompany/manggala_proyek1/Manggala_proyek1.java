ae81803298057f0f35b0396597fca98eef642458
manggalaayudha <bagusmanggalayudha.9@gmail.com>
2026-09-01 23:05:12 +0700

    Commit Tugas Modul 2 Prak PBO

---
+++ manggala_proyek1/src/main/java/com/mycompany/manggala_proyek1/Manggala_proyek1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.manggala_proyek1;
+
+public class Manggala_proyek1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Bagus Manggala Yudha");
+        System.out.println("NPM: 2505060057");
+        System.out.println("Alamat: Magelang");
+        System.out.println("No_HP: 081234567890");
+    }
+}
